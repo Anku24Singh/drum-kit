5c9af94d425a10696a81b2d3fa78ffc1d25692db
Anku Kumari <ankusinghcps@gmail.com>
2024-01-05 19:58:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// document.querySelector("button").addEventListener("click",handleClick);
+
+function handleClick1(){
+    let mysound=new Audio('crash.mp3');
+    mysound.play();
+}
+function handleClick2(){
+    let mysound=new Audio('kick-bass.mp3');
+    mysound.play();
+}
+function handleClick3(){
+    let mysound=new Audio('snare.mp3');
+    mysound.play();
+}
+function handleClick4(){
+    let mysound=new Audio('tom-1.mp3');
+    mysound.play();
+}
+function handleClick5(){
+    let mysound=new Audio('tom-2.mp3');
+    mysound.play();
+}
+function handleClick6(){
+    let mysound=new Audio('tom-3.mp3');
+    mysound.play();
+}
+function handleClick7(){
+    let mysound=new Audio('tom-4.mp3');
+    mysound.play();
+}
